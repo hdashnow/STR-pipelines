b3aa8cad2ff9442f7c872a6df7bb1f12d9787e82
Harriet Dashnow <h.dashnow@gmail.com>
2015-05-11 16:49:48 +1000

    initial commit of RepeatSeq pipeline

---
+++ repeatseq/RepeatSeq_pipeline.groovy
@@ -0,0 +1,27 @@
+// Paths relative to the analysis directory
+load "../bpipe.config"
+// RepeatSeq installation
+REPEATSEQ="/vlsci/VR0320/hdashnow/git/repeatseq"
+
+// Reference data
+REF="/vlsci/VR0002/shared/Reference_Files/GATK_bundle_Refs/hg19/ucsc.hg19.fasta" // Genome
+REGIONS="$REPEATSEQ/regions/hg19.2014.regions.gz"
+// STRs in the format required by RepeatSeq
+
+@transform("vcf")
+genotype = {
+
+    doc "Genotype STRs using RepeatSeq"
+
+    produce("$output.vcf") {
+        exec """
+            $REPEATSEQ/repeatseq $input.bam $REF $REGIONS
+        """
+    }
+}
+
+run {
+    "%.bam" * [
+        genotype
+    ]
+}
