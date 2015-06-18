b16d29d9b49ae23682d759ed94b8544079013a31
Harriet Dashnow <h.dashnow@gmail.com>
2015-06-19 09:29:48 +1000

    Point to unzipped version of .regions

--- repeatseq/RepeatSeq_pipeline.groovy
+++ repeatseq/RepeatSeq_pipeline.groovy
@@ -5,7 +5,7 @@ REPEATSEQ="/vlsci/VR0320/hdashnow/git/repeatseq"
 
 // Reference data
 REF="/vlsci/VR0002/shared/Reference_Files/GATK_bundle_Refs/hg19/ucsc.hg19.fasta" // Genome
-REGIONS="$REPEATSEQ/regions/hg19.2014.regions.gz"
+REGIONS="$REPEATSEQ/regions/hg19.2014.regions"
 // STRs in the format required by RepeatSeq
 
 @transform("vcf")
