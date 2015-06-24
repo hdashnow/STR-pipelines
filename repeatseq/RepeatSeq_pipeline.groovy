82ebb439b296432d9d63ef9b947ff76cb395f12a
Harriet Dashnow <h.dashnow@gmail.com>
2015-06-24 11:03:09 +1000

    fixed transform statement in repeatseq command

--- repeatseq/RepeatSeq_pipeline.groovy
+++ repeatseq/RepeatSeq_pipeline.groovy
@@ -1,5 +1,5 @@
 // Paths relative to the analysis directory
-load "../bpipe.config"
+load "/vlsci/VR0320/hdashnow/git/STR-pipelines/repeatseq/bpipe.config"
 // RepeatSeq installation
 REPEATSEQ="/vlsci/VR0320/hdashnow/git/repeatseq"
 
@@ -8,12 +8,11 @@ REF="/vlsci/VR0002/shared/Reference_Files/GATK_bundle_Refs/hg19/ucsc.hg19.fasta"
 REGIONS="$REPEATSEQ/regions/hg19.2014.regions"
 // STRs in the format required by RepeatSeq
 
-@transform("vcf")
 genotype = {
 
     doc "Genotype STRs using RepeatSeq"
 
-    produce("$output.vcf") {
+    from("bam") transform("bam.vcf") {
         exec """
             $REPEATSEQ/repeatseq $input.bam $REF $REGIONS
         """
