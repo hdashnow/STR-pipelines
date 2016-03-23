b3278c27b60b7673f88865eb2659c00c7250f9f3
Harriet Dashnow <h.dashnow@gmail.com>
2016-03-23 15:46:01 +1100

    generate header vcf on the fly, test writing a new vcf record from scratch

--- simulate_reads/generate_stutter_vcfs.py
+++ simulate_reads/generate_stutter_vcfs.py
@@ -126,12 +126,23 @@ def parse_bed(bedfilename, bed_dict = {}, position_base = 1):
                 bed_dict[unique_id] = {"chr":ref_chr, "start":ref_start, "stop":ref_stop, "name":name}
     return bed_dict
 
-def get_vcf_writer(template, vcf_outfile): #XXX write a temporary template vcf with correct date, source etc.
+def get_vcf_writer(vcf_outfile):
+    """Generate a vcf writer object.
+    Writes a template vcf containing header info (can be deleted afterwards?)"""
+
+    template = 'template.vcf'
+    with open(template, "w") as vcf_template:
+        vcf_template.write('##fileformat=VCFv4.1\n')
+        vcf_template.write('##source={}\n'.format('generate_stutter_vcfs.py'))
+        vcf_template.write('##reference={}\n'.format('ucsc.hg19.fasta'))
+        vcf_template.write('##INFO=<ID=RU,Number=1,Type=String,Description="Repeat Unit">\n')
+        vcf_template.write('##INFO=<ID=RL,Number=1,Type=Integer,Description="Reference Length of Repeat">\n')
+        vcf_template.write('##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">\n')
+        vcf_template.write('#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	SAMPLE\n')
+
     with open(template) as vcf_template:
         vcf_reader = vcf.Reader(vcf_template)
         vcf_writer = vcf.Writer(open(vcf_outfile, 'w'), vcf_reader)
-        for record in vcf_reader:
-            vcf_writer.write(record)
     return vcf_writer
 
 def main():
@@ -149,7 +160,13 @@ def main():
     bed_dict = {}
     #bed_dict = parse_bed(args.bed, bed_dict, position_base)
 
-    writer = get_vcf_writer('ATXN1.simulation.vcf', vcf_out)
+    vcf_writer = get_vcf_writer(vcf_out)
+
+    record = vcf.model._Record(CHROM='chr6', POS=16243709, ID='.', REF='T',
+                                ALT=[vcf.model._Substitution('TGCTGCTGCTGCTGCTGCTGCTGCTGCTGCT')],
+                                QUAL='.', FILTER='PASS', INFO={'RU':'GCT', 'RL':29},
+                                FORMAT='.', sample_indexes=[], samples=None)
+    vcf_writer.write_record(record)
 
 if __name__ == '__main__':
     main()
